9db1a682de1a54062d32abe852646163f8de7898
andrew chang <andrewchang@andrews-MacBook-Air.local>
2023-08-09 11:19:43 +0900

    chat history works

--- bot/chatgpt.py
+++ bot/chatgpt.py
@@ -1,49 +1,52 @@
 import os
 import sys
-import constants
-from langchain.document_loaders import TextLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import OpenAI
+
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.chat_models import ChatOpenAI
-import Levenshtein
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
+import constants
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-# Get input from command line
-user_query = sys.argv[1]
+# Enable to save to disk & reuse the model (for repeated queries on the same data)
+PERSIST = False
 
-# Search for the closest matching file with a name that matches user_query
-matching_file_path = None
-min_distance = float('inf')
-for file_name in os.listdir("./files/"):
-    if file_name.endswith(".txt"):
-        distance = Levenshtein.distance(user_query.lower(), file_name.lower())
-        if distance < min_distance:
-            min_distance = distance
-            matching_file_path = os.path.join("./files/", file_name)
+query = None
+if len(sys.argv) > 1:
+  query = sys.argv[1]
 
-if matching_file_path:
-    # Read the content of the matching file
-    with open(matching_file_path, 'r') as file:
-        file_content = file.read()
-    
-    # Create a TextLoader instance with the filename as document ID
-    loader = TextLoader(matching_file_path)
-    
-    # Create an index using VectorstoreIndexCreator
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    
-    # Provide a system message for the interview
-    system_prompt = f"You are an interviewer. You will be given a name and I need you to make 5 customized interview questions based on the resume in '{matching_file_path}' if there isn't a file that matches that exactly then say A file with that name can't be found."
-    
-    # Combine the system message and user query
-    combined_query = f"{system_prompt} {user_query}"
-    
-    # Query the index
-    llm = ChatOpenAI(model_name="gpt-4", temperature=0.8)
-    questions = index.query(combined_query, llm)
-    
-    # Print the generated interview questions
-    print(questions)
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
 else:
-    print(f"No matching .txt file found in 'files' directory for '{user_query}'.")
+  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+  loader = DirectoryLoader("files/")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+  llm=ChatOpenAI(model="gpt-4", temperature=0.9),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
+chat_history = []
+while True:
+  if not query:
+    query = input("Prompt: ")
+  if query in ['quit', 'q', 'exit']:
+    sys.exit()
+  result = chain({"question": query, "chat_history": chat_history})
+  print(result['answer'])
+
+  chat_history.append((query, result['answer']))
+  query = None
